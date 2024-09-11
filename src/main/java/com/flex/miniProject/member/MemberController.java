3591571588a8e876e0f8114df602aeaa26f2c8f6
이민주 <minjulv06101@gmail.com>
2024-09-11 12:05:45 +0900

    dbcp 세팅완료

--- src/main/java/com/flex/miniProject/member/MemberController.java
+++ src/main/java/com/flex/miniProject/member/MemberController.java
@@ -1,5 +1,6 @@
 package com.flex.miniProject.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,9 +9,20 @@ import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MemberController {
+
+    @Autowired
+    private MemberDAO mDAO;
+
     @RequestMapping(value = "/member", method= RequestMethod.GET)
     public String member(HttpServletRequest req) {
-        req.setAttribute("contentPage", "../member/member.jsp");
+        req.setAttribute("contentPage", "../member/signup.jsp");
+        return "main/main";
+    }
+
+    @RequestMapping(value = "/member.signup", method= RequestMethod.POST)
+    public String doSignup(bizone_member m, HttpServletRequest req) {
+        mDAO.signup(m, req);
+        req.setAttribute("contentPage", "../member/success.jsp");
         return "main/main";
     }
 }
