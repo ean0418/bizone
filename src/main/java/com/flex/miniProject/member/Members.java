0a04619d768b202abec26aadcc5c906fbfc927f1
이민주 <minjulv06101@gmail.com>
2024-09-11 18:05:31 +0900

    진짜진짜 성공

--- src/main/java/com/flex/miniProject/member/Members.java
+++ src/main/java/com/flex/miniProject/member/Members.java
@@ -3,21 +3,21 @@ package com.flex.miniProject.member;
 import java.util.List;
 
 public class Members {
-    private List<bizone_member> member;
+    private List<Bizone_member> member;
 
     public Members() {
 
     }
 
-    public Members(List<bizone_member> member) {
+    public Members(List<Bizone_member> member) {
         this.member = member;
     }
 
-    public List<bizone_member> getMember() {
+    public List<Bizone_member> getMember() {
         return member;
     }
 
-    public void setMember(List<bizone_member> member) {
+    public void setMember(List<Bizone_member> member) {
         this.member = member;
     }
 }
