54eec08747a3f8fcbe42985c9014669eff359839
ean <abhjs1337@naver.com>
2024-09-20 17:13:39 +0900

    9월20일

--- src/main/java/com/flex/miniProject/member/MemberController.java
+++ src/main/java/com/flex/miniProject/member/MemberController.java
@@ -1,13 +1,24 @@
 package com.flex.miniProject.member;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UrlPathHelper;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.UnsupportedEncodingException;
+import java.util.*;
 
 @Controller
 public class MemberController {
@@ -15,14 +26,13 @@ public class MemberController {
     @Autowired
     private MemberDAO mDAO;
 
+
     @RequestMapping(value = "/member.step1", method = RequestMethod.GET)
     public String showStep1() {
         return "member/joinStep1"; // agreement step
     }
-    @RequestMapping(value = "/kakao.login", method = RequestMethod.GET)
-    public String kakao_login() {
-        return "member/kakao_login"; // agreement step
-    }
+
+
     @RequestMapping(value ="/step1",  method = RequestMethod.POST)
     public String handleStep1() {
         return "redirect:/member/signup"; // signup로 리다이렉트
@@ -102,4 +112,45 @@ public class MemberController {
         req.setAttribute("contentPage", "../member/info.jsp");
         return "main/index";
     }
+
+    @RequestMapping(value = "/kakao.login", method = RequestMethod.GET)
+    public String loginpage_kakao_callback(HttpServletRequest request, HttpServletResponse response,
+                                           HttpSession session, Model model) throws Exception {
+
+        // URL Path 및 파라미터 처리
+        UrlPathHelper urlPathHelper = new UrlPathHelper();
+        String originalURL = urlPathHelper.getOriginatingRequestUri(request);
+        Map<String, String[]> paramMap = request.getParameterMap();
+        Iterator<String> keyData = paramMap.keySet().iterator();
+
+        // CommonData 대신 HashMap 사용
+        Map<String, String> dto = new HashMap<>();
+        while (keyData.hasNext()) {
+            String key = keyData.next();
+            String[] value = paramMap.get(key);
+            dto.put(key, value[0]);
+        }
+
+        // Access Token 요청
+        String url = "https://kauth.kakao.com/oauth/token";
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        LinkedMultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.add("client_id", "412e7727ffd0b8900060854044814879");
+        String redirect_url = "http://localhost/kakao.login";
+        map.add("redirect_uri", redirect_url);
+        map.add("grant_type", "JGu5TGA6vgs4o_623UC0EKUkFgzabCH8WBHPwEm-l0_1fOKcLa3m5wAAAAQKKcleAAABkg3I7-QFVMIyByjmyg");
+        map.add("code", dto.get("code")); // 인가 코드 추가
+
+        HttpEntity<LinkedMultiValueMap<String, String>> request2 = new HttpEntity<>(map, headers);
+        Map<String, Object> response2 = restTemplate.postForObject(url, request2, Map.class);
+
+        // Access Token 출력
+        model.addAttribute("access_token", response2.get("access_token"));
+
+        return "/kakao.login";
+    }
+
 }
