6328f07699b4a8e0524fedc78e1292e6afac9ad0
이민주 <minjulv06101@gmail.com>
2024-09-23 09:54:54 +0900

    dependency 수정 및 실행 가능 버전

--- src/main/java/com/flex/miniProject/member/EmailController.java
+++ src/main/java/com/flex/miniProject/member/EmailController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,7 +20,9 @@ public class EmailController {
     private EmailService eServices;
 
     @RequestMapping(value = "/email.send", method = RequestMethod.POST)
-    public Map<String,Object> sendEmail(@RequestBody Map<String,Object> inputMap) {
+    public Map<String,Object> sendEmail(HttpServletRequest req, HttpServletResponse res, @RequestBody Map<String, Object> inputMap) throws UnsupportedEncodingException {
+        req.setCharacterEncoding("utf-8");
+        res.setCharacterEncoding("utf-8");
         Map<String, Object> returnMap = new HashMap<>();
         returnMap.put("code", eServices.joinEmail((String) inputMap.get("email")));
         return returnMap;
